b0251c9e1f8ca87da51320c650a7a46f38c560e2
johns <johns643212@gmail.com>
2024-06-26 23:02:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-violet-700 bg-amber-50 border-red-700 '>Hello 
+        JOHNS
+
+      </h1>
+    </div>
+  )
+}
